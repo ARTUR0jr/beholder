d69e13498b2cd3e16802c2b20def5525b9290764
Arturo Saavedra Jr <97508388+ARTUR0jr@users.noreply.github.com>
2024-06-10 15:09:58 +0800

    script for menu

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
+const menuButton = document.getElementById('menu-button');
+const mobileNav = document.getElementById('mobile-nav');
+
+menuButton.addEventListener('click', function() {
+  mobileNav.classList.toggle('show'); // Toggle 'show' class for visibility
+});
 
